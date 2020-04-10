84c15d374d0dfe14dc5f3aa08039aac8a9165d81
wangmaoxiong <2268461750@qq.com>
2020-04-10 16:11:51 +0800

    系统配置类，专门用于提供 Bean ，然后交由 Spring Boot 容器管理.

---
+++ src/main/java/com/wmx/quartzjdbc/config/BeanConfig.java
@@ -0,0 +1,30 @@
+package com.wmx.quartzjdbc.config;
+
+import org.quartz.Scheduler;
+import org.springframework.boot.autoconfigure.quartz.QuartzAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+
+import javax.annotation.Resource;
+
+/**
+ * @author wangmaoxiong
+ * @version 1.0
+ * @date 2020/4/9 16:49
+ */
+@Configuration
+public class BeanConfig {
+    /**
+     * {@link QuartzAutoConfiguration } 类中自动将 {@link SchedulerFactoryBean }对象交由 Spring 容器管理.
+     * 所以可以直接获取 SchedulerFactoryBean，然后根据它来获取 {@link Scheduler}。
+     * 为了方便将 {@link Scheduler} 也交由容器管理.
+     */
+    @Resource
+    private SchedulerFactoryBean schedulerFactoryBean;
+
+    @Bean
+    public Scheduler scheduler() {
+        return schedulerFactoryBean.getScheduler();
+    }
+}
