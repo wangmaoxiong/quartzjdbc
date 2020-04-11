6a7964c8bc1c6d5539e588c173d7907084f856be
wangmaoxiong <2268461750@qq.com>
2020-04-11 18:47:37 +0800

    提供 RestTemplate 组件.

--- src/main/java/com/wmx/quartzjdbc/config/BeanConfig.java
+++ src/main/java/com/wmx/quartzjdbc/config/BeanConfig.java
@@ -4,6 +4,7 @@ import org.quartz.Scheduler;
 import org.springframework.boot.autoconfigure.quartz.QuartzAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 import org.springframework.web.client.RestTemplate;
 
@@ -29,8 +30,17 @@ public class BeanConfig {
         return schedulerFactoryBean.getScheduler();
     }
 
+    /**
+     * 设置 http 连接工厂，然后将 RestTemplate 实例交由 Spring 容器管理
+     *
+     * @return
+     */
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        //先设置 http 连接工厂的连接超时时间为 30 秒，读取超时时间为 120 秒
+        OkHttp3ClientHttpRequestFactory okRequestFactory = new OkHttp3ClientHttpRequestFactory();
+        okRequestFactory.setConnectTimeout(30 * 1000);
+        okRequestFactory.setReadTimeout(120 * 1000);
+        return new RestTemplate(okRequestFactory);
     }
 }
